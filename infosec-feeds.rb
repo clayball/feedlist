74db086b9f7148b5fa58fc3a960245ef144de947
Clay Wells <cwells73@gmail.com>
2014-12-05 18:35:07 -0500

    moved fetch of RSS feeds into a function.

--- infosec-feeds.rb
+++ infosec-feeds.rb
@@ -1,6 +1,8 @@
 #!/usr/bin/env ruby
 
-# Fetch XML based feeds from various information security
+# Fetch XML based RSS feeds from various information security websites.
+# - save the XML file for processing later.. possibly in various ways/functions
+#
 #
 # The following sources are fetched
 # ---------------------------------
@@ -13,6 +15,11 @@
 #   using the Security Content Automation Protocol (SCAP). This data enables automation of vulnerability
 #   management, security measurement, and compliance. NVD includes databases of security checklists,
 #   security related software flaws, misconfigurations, product names, and impact metrics.
+# - TODO: add more sources
+#         More sources will allow us to collorelate data across sources and possibly make decisions based
+#         on patterns found across many sources. For example, an SSH vulnerability is blowing up everywhere..
+#         ..we should pay special attention to this vulnerability ASAP.
+
 
 # TODO: migrate to nokogiri instead.
 require 'nokogiri'
@@ -25,21 +32,40 @@ xmlfeeds = {
 	# add more feeds here
 }
 
-fout = File.new("feeds.log", "w")
+####################
+# Functions        #
+####################
 
-# Loop through xmlfeeds and fetch each RSS XML file.. we can play with these file locally later.
-xmlfeeds.each_pair do |key, value|
-	#fout.print "Start: #{Time.now}\n"
-	xmlout = File.new("xml/#{key}.xml", "w")
-	print "[+] Fetching #{key} at #{value}\n"
-	doc = Nokogiri::HTML(open("#{value}"))
-	xmlout.print doc
-	print "[+] #{key} file saved to xml/#{key}.xml\n"
+def get_feeds(xmlfeeds)
+	print "[+] Fetching feeds.. \n"
+	# Loop through xmlfeeds and fetch each RSS XML file.. we can play with these file locally later.
+	xmlfeeds.each_pair do |key, value|
+		#fout.print "Start: #{Time.now}\n"
+		xmlout = File.new("xml/#{key}.xml", "w")
+		print "[+] Fetching #{key} at #{value}\n"
+		doc = Nokogiri::HTML(open("#{value}"))
+		xmlout.print doc
+		print "[+] #{key} file saved to xml/#{key}.xml\n"
+	end
+	print "[+] Fetch complete\n"
+	return "success"
 end
 
-fout.print "[+] Fetch complete: #{Time.now}\n"
+# End Functions
+####################
+
+####################
+# Main             #
+####################
+
+get_feeds_result = get_feeds(xmlfeeds)
+
+print "[debug] get_feeds_result: #{get_feeds_result}\n"
 
 # Use nokogiri to open and parse the XML files we downloaded.
 #f = File.open(".xml")
 #doc = Nokogiri::XML(f)
 #f.close
+
+# End Main
+####################
