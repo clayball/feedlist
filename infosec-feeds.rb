6fe9dda62a3e1f63b7e57a396cf035d400a908bc
Michael Dur <michaeljdur@gmail.com>
2014-12-06 11:55:16 -0500

    initial refactor to class

--- infosec-feeds.rb
+++ infosec-feeds.rb
@@ -1,52 +1,37 @@
 #!/usr/bin/env ruby
-
-# Fetch XML based RSS feeds from various information security websites.
-# - save the XML file for processing later.. possibly in various ways/functions
-#
-# The original idea was to fetch feeds specifically related to alerts and advisories related to software
-# vulnerabilities. It might be useful to include other classifications of sources, e.g., favorite blogs,
-# news, and (cough..cough) vendors. 
-#
-# The following sources are fetched
-# ---------------------------------
-#
-# - US-CERT is part of DHS' National Cybersecurity and Communications Integration Center (NCCIC).
-#   The Department of Homeland Security's United States Computer Emergency Readiness Team (US-CERT)
-#   leads efforts to improve the nation's cybersecurity posture, coordinate cyber information sharing,
-#   and proactively manage cyber risks to the Nation while protecting the constitutional rights of Americans.
-# - NVD is the U.S. government repository of standards based vulnerability management data represented
-#   using the Security Content Automation Protocol (SCAP). This data enables automation of vulnerability
-#   management, security measurement, and compliance. NVD includes databases of security checklists,
-#   security related software flaws, misconfigurations, product names, and impact metrics.
-# - Cisco Security Advisories
-#   PSIRT manages the receipt, investigation, and public reporting of security vulnerability information related
-#   to Cisco products and networks. Subscribe to the Cisco Security Advisory RSS Feed to receive information
-#   about significant security issues that directly involve Cisco products and require an upgrade, fix, or other
-#   customer action.
-# - TODO: add more sources
-#         More sources will allow us to collorelate data across sources and possibly make decisions based
-#         on patterns found across many sources. For example, an SSH vulnerability is blowing up everywhere..
-#         ..we should pay special attention to this vulnerability ASAP.
-
-
-# TODO: migrate to nokogiri instead.
 require 'nokogiri'
 require 'open-uri'
 
-# create xml/feed_categories
-feed_categories = %w[alert vendor news blog]
-Dir.mkdir('xml') unless Dir.exists?('xml')
-feed_categories.each do |f|
-    Dir.mkdir('xml/'+f) unless Dir.exists?('xml'+f)
+class Feed
+  attr_accessor :name,:type,:url,:file
+
+  def initialize(name, type, url)
+    @name = name
+    @type = type
+    @url = url
+    Dir.mkdir('xml') unless Dir.exists?('xml')
+    Dir.mkdir('xml/'+@type) unless Dir.exists?('xml/'+@type)
+    @file = "#{@type}_#{@name}.xml"
+  end
+
+  def get_feed
+    output_path = File.expand_path(File.dirname(__FILE__)) + '/xml/' + type + '/'
+    output_file = File.new(output_path+@file, "w")
+    puts "[+] Fetching #{@name}\n    from: #{@url}"
+    output_file.print Nokogiri::XML(open(@url))
+    output_file.close
+    puts "[+] #{@name} saved to #{@file}"
+  end
 end
 
-####################
+feed = Feed.new('cert', 'alert', 'http://www.us-cert.gov/ncas/alerts.xml')
+feed.get_feed
+
 # XML feed hashes based on type, e.g., vulnerability alerts, infosec news, etc
 alertfeeds = {
 	alert_cert: 'https://www.us-cert.gov/ncas/alerts.xml',
 	alert_nvd: 'https://nvd.nist.gov/download/nvd-rss.xml',
 	alert_sans: 'https://isc.sans.edu/rssfeed.xml'
-	# add more feeds
 }
 
 vendorfeeds = {
@@ -62,44 +47,3 @@ blogfeeds = {
 	blog_moxie: 'http://www.thoughtcrime.org/blog/rss.xml',
 	blog_krebs: 'http://krebsonsecurity.com/feed/'
 }
-
-####################
-# Functions        #
-####################
-def get_feeds(feeds,type)
-	print "[+] Fetching #{type} feeds.. \n"
-	# Loop through xmlfeeds and fetch each RSS XML file.. we can play with these file locally later.
-	feeds.each_pair do |key, value|
-		#fout.print "Start: #{Time.now}\n"
-		xmlout = File.new("xml/#{type}/#{key}.xml", "w")
-		print "[+] Fetching #{key} at #{value}\n"
-		doc = Nokogiri::XML(open("#{value}"))
-		xmlout.print doc
-		print "[+] ..#{key} file saved to xml/#{type}/#{key}.xml\n"
-	end
-	print "[+] Fetch complete\n"
-	return "success"
-end
-# End Functions
-####################
-
-####################
-# Main             #
-####################
-
-get_feeds_result = get_feeds(alertfeeds, 'alert')
-print "[info] alert feeds result: #{get_feeds_result}\n"
-
-get_feeds_result = get_feeds(vendorfeeds, 'vendor')
-print "[info] vendor feeds result: #{get_feeds_result}\n"
-
-get_feeds_result = get_feeds(newsfeeds, 'news')
-print "[info] news feeds result: #{get_feeds_result}\n"
-
-get_feeds_result = get_feeds(blogfeeds, 'blog')
-print "[info] blog feeds result: #{get_feeds_result}\n"
-
-# Use nokogiri to open and parse the XML files we downloaded.
-#f = File.open(".xml")
-#doc = Nokogiri::XML(f)
-#f.close
