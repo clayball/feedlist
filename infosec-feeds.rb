5fac902992a15bb8b55e482269a7f46731ec4f3f
Michael Dur <michaeljdur@gmail.com>
2014-12-13 11:01:27 -0500

    moved making directories to another method, removed puts so this can run silently

--- infosec-feeds.rb
+++ infosec-feeds.rb
@@ -3,36 +3,42 @@ require 'open-uri'
 require 'yaml'
 
 class Feed
-  attr_accessor :name, :type, :url, :file
+  attr_accessor :name, :type, :url, :file_name, :output_path
 
   def initialize(name, type, url)
     @name, @type, @url = name, type, url
-    Dir.mkdir('xml') unless Dir.exist?('xml')
-    Dir.mkdir('xml/' + @type) unless Dir.exist?('xml/' + @type)
-    @file = "#{@type}_#{@name}.xml"
+    @file_name = "#{@type}_#{@name}.xml"
+    @output_path = File.expand_path(File.dirname(__FILE__)) +
+                   '/xml/' + @type + '/'
   end
 
-  def get_feed
-    output_path = File.expand_path(File.dirname(__FILE__)) + '/xml/' + type + '/'
-    output_file = File.new(output_path + @file, 'w')
-    puts "[+] Fetching #{@name} from: #{@url}"
-    # being rescue block for getting?
-    File.open(output_file, 'wb') do |file|
-      file.write open(url).read
+  def mkdirs
+    Dir.mkdir('xml') unless Dir.exist?('xml')
+    Dir.mkdir('xml/' + @type) unless Dir.exist?('xml/' + @type)
+    self
+  end
+
+  def download_feed
+    mkdirs
+    output_file = File.new(@output_path + @file_name, 'w')
+    begin
+      File.open(output_file, 'wb') do |file|
+        file.write open(url).read
+      end
+    rescue StandardError
+      STDERR.puts "Failed to download #{@url}:\n #{$ERROR_INFO}"
     end
-    # rescue?
-    # end
-    puts "[+] #{@name} saved to #{output_path + @file}"
+    self
   end
 end
 
 feeds = begin
-           YAML.load(File.open('feeds.yml'))
-         rescue ArgumentError => e
-           puts "Could not parse YAML: #{e.message}"
-         end
+          YAML.load(File.open('feeds.yml'))
+        rescue ArgumentError => e
+          puts "Could not parse YAML: #{e.message}"
+        end
 
 feeds.each do |value|
   f = Feed.new(value[:name], value[:type], value[:url])
-  f.get_feed
+  f.download_feed
 end
