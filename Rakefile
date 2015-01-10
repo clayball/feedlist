4af2593f74a22e2a6ba7611cc35af31fcda62945
Clay Wells <cwells73@gmail.com>
2015-01-09 22:17:25 -0500

    adding create_feedlist task.

--- Rakefile
+++ Rakefile
@@ -10,13 +10,14 @@ task :createdb do
 end
 
 desc "Backup our database"
-task :createdb do
+task :baskupdb do
 	puts "[+] TODO Task: Backup our database"
 end
 
 # This is more for my personal use for now. Not sure if this task will be permanent.
 desc "Create feed listing HTML"
-task :feedlist do
+task :create_feedlist do
 	puts "[+] TODO Task: Creating feed listing HTML file."
+	ruby 'tasks/create-feedlist.rb'
 end
 
---
+++ tasks/create-feedlist.rb
@@ -0,0 +1,24 @@
+require 'yaml'
+
+feed_sources = 
+	begin
+    YAML.load(File.open('config/feed_sources.yml'))
+  rescue ArgumentError => e
+    puts "Could not parse YAML: #{e.message}"
+  end
+
+File.open("logs/feedlist.htm", "w") do |f|
+  f.puts "<!DOCTYPE html>"
+  f.puts "<html>"
+  f.puts "<head><title>InfoSec Feeds List</title></head>"
+  f.puts "<body>"
+  f.puts "<h2>InfoSec Feeds List</h2>"
+  f.puts "<ul>"
+	feed_sources.each do |value|
+  	f.puts "<li><a href='#{value[:name]}.htm'>#{value[:name]}</a>"
+ end # each
+ f.puts "</ul>"
+ f.puts "</body></html>"
+end # File.open loop
+
+puts "[+] feedlist.htm has been created."
