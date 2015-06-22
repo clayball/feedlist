2e855e404cb4baba512c975ff313ace21e6268c4
Clay Wells <cwells73@gmail.com>
2015-06-22 14:27:41 -0400

    minor udates.

--- lib/feedlist/feed.rb
+++ lib/feedlist/feed.rb
@@ -3,15 +3,14 @@
 ###############################################################################
 # This file is part of feedlist
 #
-# FeedLists is free software: you can redistribute it and/or modify it under
+# FeedList is free software: you can redistribute it and/or modify it under
 # the terms of the GNU General Public License as published by the Free
 # Software Foundation, either version 3 of the License, or (at your option)
 # any later version.
 #
-# Infosec-Feeds is distributed in the hope that it will be useful, but WITHOUT
-# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
-# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
-# more details.
+# FeedList is distributed in the hope that it will be useful, but WITHOUT ANY
+# WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR
+# A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
 # along with feedlist.  If not, see <http://www.gnu.org/licenses/>.
